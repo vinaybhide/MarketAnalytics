79140bc8ac59fa031bd34a94bc30425fef935868
Vinay Bhide <vinaybhide@hotmail.com>
2022-11-24 15:05:25 +0530

    V3

--- Data/DBContext.cs
+++ Data/DBContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MarketAnalytics.Models;
 using System.Reflection.Metadata;
+using System.Diagnostics;
 
 namespace MarketAnalytics.Data
 {
@@ -17,8 +18,9 @@ namespace MarketAnalytics.Data
 
         public DbSet<MarketAnalytics.Models.StockMaster> StockMaster { get; set; } = default!;
         public DbSet<MarketAnalytics.Models.StockPriceHistory> StockPriceHistory { get; set; } = default!;
+        public DbSet<MarketAnalytics.Models.Portfolio_Master> PORTFOLIO_MASTER { get; set; } = default!;
         public DbSet<MarketAnalytics.Models.PORTFOLIO> PORTFOLIO { get; set; } = default!;
-
+        public DbSet<MarketAnalytics.Models.PORTFOLIOTXN> PORTFOLIOTXN { get; set; } = default!;
         public DbSet<MarketAnalytics.Models.V20_CANDLE_STRATEGY> V20_CANDLE_STRATEGY { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -26,6 +28,22 @@ namespace MarketAnalytics.Data
             modelBuilder.Entity<StockMaster>().ToTable("StockMaster");
             //modelBuilder.Entity<StockPriceHistory>().ToTable("StockPriceHistory");
             modelBuilder.Entity<StockPriceHistory>().ToTable("StockPriceHistory").Navigation(e => e.StockMaster).AutoInclude();
+
+            modelBuilder.Entity<Portfolio_Master>().ToTable("PORTFOLIO_MASTER");
+
+            modelBuilder.Entity<PORTFOLIOTXN>()
+                 .HasOne(d => d.portfolioMaster)
+                 .WithMany(dm => dm.collectionTxn)
+                 .HasForeignKey(dkey => dkey.PORTFOLIO_MASTER_ID);
+
+            modelBuilder.Entity<PORTFOLIOTXN>()
+                 .HasOne(d => d.stockMaster)
+                 .WithMany(dm => dm.collectionTxn)
+                 .HasForeignKey(dkey => dkey.StockMasterID);
+
+            modelBuilder.Entity<PORTFOLIOTXN>().ToTable("PORTFOLIOTXN").Navigation(e => e.stockMaster).AutoInclude();
+            modelBuilder.Entity<PORTFOLIOTXN>().ToTable("PORTFOLIOTXN").Navigation(e => e.portfolioMaster).AutoInclude();
+
 
             //Autoinclude will automatically include the StockMaster object
             modelBuilder.Entity<PORTFOLIO>().ToTable("PORTFOLIO").Navigation(e => e.StockMaster).AutoInclude();
