de0d7d2c240f2ae7d93495869873b83ffb4b4630
Vinay Bhide <vinaybhide@hotmail.com>
2022-12-21 15:23:37 +0530

    V14

---
+++ Models/BearishEngulfingModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace MarketAnalytics.Models
+{
+    public class BEARISH_ENGULFING
+    {
+        [Key]
+        public int BEARISH_ENGULFING_ID { get; set; }
+        
+        [Required]
+        [DisplayName("Buy Ref Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = false)]
+
+        public DateTime BUY_CANDLE_DATE { get; set; } = default(DateTime);
+
+        [Required]
+        [DisplayName("Sell Ref Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = false)]
+
+        public DateTime SELL_CANDLE_DATE { get; set; } = default(DateTime);
+
+        [Required]
+        public double BUY_PRICE { get; set; } = default(double);
+        [Required]
+        public double SELL_PRICE { get; set; } = default(double);
+        public int StockMasterID { get; set; }
+        public StockMaster StockMaster { get; set; }
+    }
+}
