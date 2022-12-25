37eaedfca71e334abe11be77e5f814a4cffee13c
Vinay Bhide <vinaybhide@hotmail.com>
2022-12-25 20:01:30 +0530

    V15

--- Pages/History/Index.cshtml.cs
+++ Pages/History/Index.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis;
+using System.Diagnostics;
 
 namespace MarketAnalytics.Pages.History
 {
@@ -20,6 +21,7 @@ namespace MarketAnalytics.Pages.History
         private readonly IConfiguration Configuration;
 
         public List<SelectListItem> symbolList { get; set; }
+        public List<SelectListItem> groupList { get; set; }
 
         public string PriceDateSort { get; set; }
         public int? CurrentID { get; set; }
@@ -37,10 +39,11 @@ namespace MarketAnalytics.Pages.History
             _context = context;
             Configuration = configuration;
             symbolList = new List<SelectListItem>();
+            groupList = new List<SelectListItem>();
         }
 
-        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex, 
-                                    int? id, bool?refreshAll)
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex,
+                                    int? id, bool? refreshAll)
         {
             if (_context.StockPriceHistory != null)
             {
@@ -54,6 +57,21 @@ namespace MarketAnalytics.Pages.History
                                                     Text = a.Symbol
                                                 }
                                             ).ToList();
+                groupList.Clear();
+
+                SelectListItem selectAll = new SelectListItem("-- Select All --", "-99");
+                groupList.Add(selectAll);
+
+                selectAll = new SelectListItem("Refresh V40", "-98");
+                groupList.Add(selectAll);
+
+                selectAll = new SelectListItem("Refresh V40N", "-97");
+                groupList.Add(selectAll);
+
+                selectAll = new SelectListItem("Refresh V200", "-96");
+                groupList.Add(selectAll);
+
+                IQueryable<StockPriceHistory> stockpriceIQ = null;
 
                 //CurrentID = id;
                 CurrentSort = sortOrder;
@@ -68,7 +86,7 @@ namespace MarketAnalytics.Pages.History
                 }
 
                 StockMasterRec = null;
-                if (id != null)
+                if ((id != null) && (id > 0))
                 {
                     CurrentID = id;
                     //var selectedRecord = await _context.StockMaster.FirstOrDefaultAsync(m => m.StockMasterID == id);
@@ -94,6 +112,22 @@ namespace MarketAnalytics.Pages.History
                         if (string.IsNullOrEmpty(lastPriceDate) == false)
                         {
                             DbInitializer.InitializeHistory(_context, StockMasterRec, StockMasterRec.Symbol, StockMasterRec.CompName, StockMasterRec.Exchange, lastPriceDate);
+
+                            DbInitializer.GetSMA_EMA_MACD_BBANDS_Table(_context, StockMasterRec, StockMasterRec.Symbol, StockMasterRec.Exchange, CurrentID,
+                                StockMasterRec.CompName, System.Convert.ToDateTime(lastPriceDate));
+
+                            DbInitializer.getRSIDataTableFromDaily(_context, StockMasterRec, StockMasterRec.Symbol, StockMasterRec.Exchange, CurrentID,
+                                                                    StockMasterRec.CompName, System.Convert.ToDateTime(lastPriceDate), period: "14");
+
+                            DbInitializer.V20CandlesticPatternFinder(_context, StockMasterRec);
+
+                            DbInitializer.GetSMA_BUYSELL(_context, StockMasterRec, StockMasterRec.Symbol, StockMasterRec.Exchange,
+                                StockMasterRec.StockMasterID, StockMasterRec.CompName, 20, 50, 200);
+
+                            DbInitializer.GetBullishEngulfingBuySellList(_context, StockMasterRec, DateTime.Today.AddDays(-180), 30, 10);
+                            DbInitializer.GetBearishEngulfingBuySellList(_context, StockMasterRec, DateTime.Today.AddDays(-180), 30, 10);
+                            DbInitializer.GetLifetimeHighLow(_context, StockMasterRec);
+
                             RefreshAllStocks = false;
                         }
                     }
@@ -101,10 +135,7 @@ namespace MarketAnalytics.Pages.History
 
                     CurrentFilter = searchString;
 
-                    //IQueryable<StockPriceHistory> stockpriceIQ = from s in _context.StockPriceHistory select s;
-
-                    IQueryable<StockPriceHistory> stockpriceIQ = _context.StockPriceHistory.Where(s => (s.StockMasterID == CurrentID));
-
+                    stockpriceIQ = _context.StockPriceHistory.Where(s => (s.StockMasterID == CurrentID));
                     if (!String.IsNullOrEmpty(searchString))
                     {
                         //stockpriceIQ = stockpriceIQ.Where(s => s.PriceDate.Date.Equals(Convert.ToDateTime(searchString).Date)
@@ -112,7 +143,38 @@ namespace MarketAnalytics.Pages.History
                         stockpriceIQ = stockpriceIQ.Where(s => (s.PriceDate.Date >= (Convert.ToDateTime(searchString).Date))
                                                                 && (s.StockMasterID == CurrentID));
                     }
+                }
+                else if((id != null) && (id < 0))
+                {
+                    RefreshHistoryForGroup((int)id);
+                    if (id == -99)
+                    {
+                        stockpriceIQ = _context.StockPriceHistory.Where(s => ((s.StockMaster.V200 == true)
+                                                                  || (s.StockMaster.V40 == true) || (s.StockMaster.V40N == true)));
+                        CompanyName = "V40+V40N+V200";
 
+                    }
+                    else if (id == -98)
+                    {
+                        stockpriceIQ = _context.StockPriceHistory.Where(s => (s.StockMaster.V40 == true));
+                        CompanyName = "V40";
+                    }
+                    else if (id == -97)
+                    {
+                        stockpriceIQ = _context.StockPriceHistory.Where(s => (s.StockMaster.V40N == true));
+                        CompanyName = "V40N";
+                    }
+                    else if (id == -96)
+                    {
+                        stockpriceIQ = _context.StockPriceHistory.Where(s => (s.StockMaster.V200 == true));
+                        CompanyName = "V200";
+                    }
+                }
+                //IQueryable<StockPriceHistory> stockpriceIQ = from s in _context.StockPriceHistory select s;
+
+
+                if (id != null)
+                {
                     switch (sortOrder)
                     {
                         case "pricedate_desc":
@@ -127,5 +189,58 @@ namespace MarketAnalytics.Pages.History
                 }
             }
         }
+
+        public void RefreshHistoryForGroup(int groupId)
+        {
+            IQueryable<StockMaster> stockmasterIQ = null;
+            string lastPriceDate = string.Empty;
+            try
+            {
+                if (groupId == -99)
+                {
+                    stockmasterIQ = _context.StockMaster.Where(s => ((s.V200 == true) || (s.V40 == true) || (s.V40N == true)));
+                }
+                else if (groupId == -98)
+                {
+                    stockmasterIQ = _context.StockMaster.Where(s => (s.V40 == true));
+                }
+                else if (groupId == -97)
+                {
+                    stockmasterIQ = _context.StockMaster.Where(s => (s.V40N == true));
+                }
+                else if (groupId == -96)
+                {
+                    stockmasterIQ = _context.StockMaster.Where(s => (s.V200 == true));
+                }
+                foreach (var item in stockmasterIQ)
+                {
+                    lastPriceDate = DbInitializer.IsHistoryUpdated(_context, item, item.StockMasterID);
+                    if (string.IsNullOrEmpty(lastPriceDate) == false)
+                    {
+                        DbInitializer.InitializeHistory(_context, item, item.Symbol, item.CompName, item.Exchange, lastPriceDate);
+
+                        DbInitializer.GetSMA_EMA_MACD_BBANDS_Table(_context, item, item.Symbol, item.Exchange, item.StockMasterID,
+                            item.CompName, System.Convert.ToDateTime(lastPriceDate));
+
+                        DbInitializer.getRSIDataTableFromDaily(_context, item, item.Symbol, item.Exchange, CurrentID,
+                                                                item.CompName, System.Convert.ToDateTime(lastPriceDate), period: "14");
+
+                        DbInitializer.V20CandlesticPatternFinder(_context, item);
+
+                        DbInitializer.GetSMA_BUYSELL(_context, item, item.Symbol, item.Exchange,
+                            item.StockMasterID, item.CompName, 20, 50, 200);
+
+                        DbInitializer.GetBullishEngulfingBuySellList(_context, item, DateTime.Today.AddDays(-180), 30, 10);
+                        DbInitializer.GetBearishEngulfingBuySellList(_context, item, DateTime.Today.AddDays(-180), 30, 10);
+                        DbInitializer.GetLifetimeHighLow(_context, item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
