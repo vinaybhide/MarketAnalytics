ac8fa173582541ae0120532dd8727debe4ba4c8a
Vinay Bhide <vinaybhide@hotmail.com>
2023-01-03 18:47:07 +0530

    V18

---
+++ Models/UpdateTracker.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace MarketAnalytics.Models
+{
+    public class UpdateTracker
+    {
+        [Key]
+        public int UpdateTracker_ID { get; set; }
+        public DateTime REF_DATE { get; set; } = default(DateTime);
+        public string TYPE { get; set; } = string.Empty;
+        public string DATA { get; set; } = string.Empty;
+    }
+}
